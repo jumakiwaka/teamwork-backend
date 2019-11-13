898200fe1787c9de13b49d3a6c89157fcf5380cf
jumakiwaka <alijuma232@gmail.com>
2019-11-13 16:32:35 +0300

    chore(database) : setting up commands for creating and deleting tables from db

---
+++ db.js
@@ -0,0 +1,71 @@
+const { Pool } = require('pg');
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL
+});
+
+pool.on('connect', () => {
+  console.log('connected to the db');
+});
+
+/**
+ * Create Tables
+ */
+const createTables = () => {
+  const queryText =
+    `CREATE TABLE IF NOT EXISTS
+      users(
+        id UUID PRIMARY KEY,
+        firstname VARCHAR(128) NOT NULL,
+        lastname VARCHAR(128) NOT NULL,
+        email VARCHAR(128) NOT NULL UNIQUE,
+        password VARCHAR(128) NOT NULL,
+        gender VARCHAR(30),
+        jobrole VARCHAR(128) NOT NULL,
+        department VARCHAR(128) NOT NULL,
+        address VARCHAR(128) NOT NULL,
+        created_date TIMESTAMP,
+        modified_date TIMESTAMP
+      )`;
+
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
+/**
+ * Drop Tables
+ */
+const dropTables = () => {
+  const queryText = 'DROP TABLE IF EXISTS users';
+  pool.query(queryText)
+    .then((res) => {
+      console.log(res);
+      pool.end();
+    })
+    .catch((err) => {
+      console.log(err);
+      pool.end();
+    });
+}
+
+pool.on('remove', () => {
+  console.log('client removed');
+  process.exit(0);
+});
+
+module.exports = {
+  createTables,
+  dropTables
+};
+
+require('make-runnable');
