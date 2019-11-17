15f4dbdb1f12047d0a33c7a608cb71b52dda777f
jumakiwaka <alijuma232@gmail.com>
2019-11-17 04:27:03 +0300

    chore(cleaningup) : removing unnecesary comments

--- controllers/gifController.js
+++ controllers/gifController.js
@@ -4,14 +4,5 @@ const gifdb = require('../models/usingDb/controllers/gifdb');
 module.exports = (req, res) => {
 
     gifdb.createGif(req, res);
-    // res.status(201).json({
-    //     "status" : "Success",
-    //     "data" : {
-    //         "gifId" : parseInt(Math.random()*1000),
-    //         "message" : "GIF image successfully posted",
-    //         "createdOn" : Date.now(),
-    //         "title" : "I am gif!",
-    //         "imageUrl" : "/img/gif2938",
-    //     }
-    // });
+    
 }
