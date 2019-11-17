bcc33db8da654f79fd1ed55c03073b22e08fe149
jumakiwaka <alijuma232@gmail.com>
2019-11-17 04:30:09 +0300

    bug(authentication) : casting req userId to an Integer

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,12 +7,13 @@ module.exports = (req, res, next) => {
         const token = req.headers.authorization.split(' ')[1];                      
         const decodedToken = jwt.verify(token, 'RANDOM_SECRET_KEY');
         const userId = decodedToken.userId;
-        if(req.body.userId && req.body.userId !== userId) {
+
+        if(req.body.userId && +req.body.userId !== userId) {
             throw new Error('Invalid Request!');
         }
         next();
     } catch (error) {
-        // console.log(error)
+        console.log(error)
         res.status(401).json({
             "status" : "error",
             "error" : error,            
