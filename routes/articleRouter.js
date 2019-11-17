fc9b4f5550987c42783b0f6a087296946e559c46
jumakiwaka <alijuma232@gmail.com>
2019-11-17 10:48:30 +0300

    chore(routing) : adding delete method to route

--- routes/articleRouter.js
+++ routes/articleRouter.js
@@ -1,11 +1,12 @@
 const express = require('express');
-const articlesController = require('../controllers/articlesController');
+const articlesCtrl = require('../controllers/articlesController');
 const auth = require('../middleware/auth');
 
 const articleRouter = express.Router();
 
-articleRouter.post('/articles', auth, articlesController.createArticle);
-articleRouter.patch('/articles/:articleId', auth, articlesController.editArticle);
+articleRouter.post('/articles', auth, articlesCtrl.createArticle);
+articleRouter.patch('/articles/:articleId', auth, articlesCtrl.editArticle);
+articleRouter.delete('/articles/:articleId', auth, articlesCtrl.deleteArticle);
 
 
 module.exports = articleRouter;
