0506de9393b64ec334ec1254ff37e2e37c1040e6
jumakiwaka <alijuma232@gmail.com>
2019-11-18 03:08:49 +0300

    chore(routing) : adding post route to create gif comment

--- routes/gifRoutes.js
+++ routes/gifRoutes.js
@@ -6,5 +6,6 @@ const gifRouter = express.Router();
 
 gifRouter.post('/gifs', auth, cloudinary_config, gifCtrl.createGif);
 gifRouter.delete('/gifs/:gifId', auth, gifCtrl.deleteGif);
+gifRouter.post('/gifs/:gifId/comment', auth, gifCtrl.createComment);
 
 module.exports = gifRouter;
